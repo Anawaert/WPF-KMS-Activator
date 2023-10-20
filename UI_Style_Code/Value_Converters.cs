4a2eda2369bce5c96eed7fc720f46f2084f23cab
Anawaert <anawaertstudio@outlook.com>
2023-10-20 10:46:19 +0800

    将UI样式的XAML文件独立出来，新增一些值转换器

---
+++ UI_Style_Code/Value_Converters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace KMS_Activator
+{
+    public class EnlargeGridSize : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double)
+            {
+                double sourceValue = (double)value;
+                return sourceValue * 2 - 25.0;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double)
+            {
+                double targetValue = (double)value;
+                return (targetValue + 25.0) / 2;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
